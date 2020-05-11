83e55347f27124bab6e04f5ca061acf0846f6c1e
Hieu Le <hieu.lethanh@veriksystems.com>
2020-05-11 10:27:54 +0700

    add build folder

---
+++ web/ui/react-app/build/precache-manifest.43a6ea2f24d9ba2619af0e23f263c990.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0201c0c773db574402fc5bb7a77c3ec6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4565ee485abe984e9a1b",
+    "url": "/static/css/main.87d1bddf.chunk.css"
+  },
+  {
+    "revision": "8a65b11293aacec5b8fd",
+    "url": "/static/js/2.894679e6.chunk.js"
+  },
+  {
+    "revision": "90565ba0b43e1b95d2876295dfd7014d",
+    "url": "/static/js/2.894679e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70b47089830b9d1c5562",
+    "url": "/static/js/3.85fc92ba.chunk.js"
+  },
+  {
+    "revision": "52e326711fc826be7d5e4f5e50321620",
+    "url": "/static/js/3.85fc92ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f857030c377bc258c363",
+    "url": "/static/js/4.5b0277ee.chunk.js"
+  },
+  {
+    "revision": "021c474445a8226579f5",
+    "url": "/static/js/5.9d3f9eca.chunk.js"
+  },
+  {
+    "revision": "7632509db3803e81c677",
+    "url": "/static/js/6.42ebb984.chunk.js"
+  },
+  {
+    "revision": "ca552c9090f2aeb449cb",
+    "url": "/static/js/7.26b25598.chunk.js"
+  },
+  {
+    "revision": "4565ee485abe984e9a1b",
+    "url": "/static/js/main.9442eb3a.chunk.js"
+  },
+  {
+    "revision": "06eb47426e94bad2e2a5",
+    "url": "/static/js/runtime-main.189fef21.js"
+  }
+]);
