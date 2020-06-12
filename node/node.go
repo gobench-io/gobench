9b5958645fc14c90821a91307aa6f904525ae822
nqd <dinh.nguyen@veriksystems.com>
2020-06-12 11:04:16 +0700

    provide singleton node variable

--- node/node.go
+++ node/node.go
@@ -1,21 +1,35 @@
 package node
 
 import (
+	"fmt"
 	"os"
 	"sync"
-	"fmt"
 
+	"github.com/gobench-io/gobench/metrics"
 	"github.com/gobench-io/gobench/scenario"
+	gometrics "github.com/rcrowley/go-metrics"
 )
 
 type status string
 
+type unit struct {
+	Title    string             // metric title
+	Type     metrics.MetricType // to know the currnt unit type
+	metricID int                // metric table foreign key
+	c        gometrics.Counter
+	h        gometrics.Histogram
+	g        gometrics.Gauge
+}
+
 type Node struct {
-	mu         sync.Mutex
-	id         string
+	mu       sync.Mutex
+	hostname string
+	pid      int
+
 	status     status
 	pluginPath string
 	vus        *scenario.Vus
+	units      map[string]unit
 }
 
 const (
@@ -23,21 +37,26 @@ const (
 	running status = "running"
 )
 
-func New() (*Node, error) {
-	hostname, err := os.Hostname()
-	if err != nil {
-		return nil, err
-	}
+// the sigleton node variable
+var node Node
+
+func init() {
+	hostname, _:= os.Hostname()
 	pid := os.Getpid()
 
-	id := fmt.Sprintf("%s-%i", hostname, pid)
-
-	return &Node{
-		id:     id,
-		status: idle,
-	}, nil
+	node = Node{
+		pid:      pid,
+		hostname: hostname,
+		status:   idle,
+	}
 }
 
+// New return the singleton node
+func New() (*Node, error) {
+	return &node, nil
+}
+
+// Load downloads the go plugin, extracts the virtual user scenario
 func (n *Node) Load(so string) error {
 	vus, err := scenario.LoadPlugin(so)
 	if err != nil {
@@ -53,6 +72,7 @@ func (n *Node) Load(so string) error {
 	return nil
 }
 
+// Run starts the preloaded plugin
 func (n *Node) Run() {
 	n.mu.Lock()
 	n.status = running
