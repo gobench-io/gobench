1370abc6da5dc929e7aeeb23d75e6aa075901a0f
nqd <dinh.nguyen@veriksystems.com>
2020-06-16 17:29:19 +0700

    update 1 to 1 mqtt example

--- examples/mqtt/1_to_1/1_to_1.go
+++ examples/mqtt/1_to_1/1_to_1.go
@@ -12,53 +12,24 @@ package main
 import (
 	"context"
 	"log"
-	"sync"
-	"time"
 
 	"github.com/gobench-io/gobench"
-	"github.com/gobench-io/gobench/web"
-	"github.com/gobench-io/gobench/workers/benchclient"
+	"github.com/gobench-io/gobench/dis"
+	"github.com/gobench-io/gobench/scenario"
 	"github.com/gobench-io/gobench/workers/mqtt"
 )
 
-func main() {
-	bench := gobench.NewBench()
-	bench.Name("mqtt 1-to-1 benchmark example")
-
-	if err := bench.Start(); err != nil {
-		log.Fatalln(err)
-	}
-
-	go web.Serve(bench, 3001)
-	go benchclient.InternalMonitor()
-
-	vu := 2000
-
-	var donewg, poolSignal sync.WaitGroup
-	donewg.Add(vu)
-	poolSignal.Add(vu)
-
-	var rate float64 = 100 // per second
-	for i := 0; i < vu; i++ {
-		gobench.SleepPoisson(rate)
-
-		go vuPool(i, &donewg, &poolSignal)
-	}
-
-	donewg.Wait()
-
-	time.Sleep(1 * time.Second)
-
-	if err := bench.Finish(); err != nil {
-		log.Printf("finish error %v\n", err)
+func Export() scenario.Vus {
+	return scenario.Vus{
+		{
+			Nu:   2000,
+			Rate: 100,
+			Fu:   f,
+		},
 	}
 }
 
-func vuPool(i int, donewg, poolSignal *sync.WaitGroup) {
-	ctx := context.Background()
-
-	defer donewg.Done()
-
+func f(ctx context.Context, vui int) {
 	opts := mqtt.NewClientOptions()
 	opts.AddBroker("192.168.2.29:1883")
 
@@ -81,7 +52,7 @@ func vuPool(i int, donewg, poolSignal *sync.WaitGroup) {
 
 	rate := 1.0 // rps
 	for j := 0; j < 60*5; j++ {
-		gobench.SleepPoisson(rate)
+		dis.SleepRatePoisson(rate)
 		_ = client.PublishToSelf(&ctx, "prefix/clients/", 0, gobench.RandomByte(150))
 	}
 
