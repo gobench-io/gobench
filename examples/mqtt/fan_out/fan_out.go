e3032c565e418dcc162c5ee3fa56732e8a716a3c
nqd <dinh.nguyen@veriksystems.com>
2020-06-17 09:40:58 +0700

    fan out

--- examples/mqtt/fan_out/fan_out.go
+++ examples/mqtt/fan_out/fan_out.go
@@ -12,57 +12,29 @@ package main
 import (
 	"context"
 	"log"
-	"sync"
 
-	"github.com/gobench-io/gobench"
-	"github.com/gobench-io/gobench/web"
-	"github.com/gobench-io/gobench/workers/benchclient"
+	"github.com/gobench-io/gobench/dis"
+	"github.com/gobench-io/gobench/scenario"
 	"github.com/gobench-io/gobench/workers/mqtt"
 )
 
-func main() {
-	bench := gobench.NewBench()
-	bench.Name("mqtt fan out benchmark example")
-
-	if err := bench.Start(); err != nil {
-		log.Fatalln(err)
-	}
-
-	go web.Serve(bench, 3001)
-	go benchclient.InternalMonitor()
-
-	subVu := 1000
-	pubVu := 1
-
-	var donewg sync.WaitGroup
-	donewg.Add(pubVu + subVu)
-
-	rate := 100.0 // per second
-
-	for i := 0; i < subVu; i++ {
-		gobench.SleepPoisson(rate)
-
-		go subVuPool(i, &donewg)
-	}
-
-	for j := 0; j < pubVu; j++ {
-		gobench.SleepPoisson(rate)
-
-		go pubVuPool(j, &donewg)
-	}
-
-	donewg.Wait()
-
-	if err := bench.Finish(); err != nil {
-		log.Printf("finish error %v\n", err)
+func Export() scenario.Vus {
+	// mqtt fan out benchmark example
+	return scenario.Vus{
+		{
+			Nu:   1,
+			Rate: 1000,
+			Fu:   subf,
+		},
+		{
+			Nu:   1,
+			Rate: 100,
+			Fu:   pubf,
+		},
 	}
 }
 
-func subVuPool(i int, donewg *sync.WaitGroup) {
-	ctx := context.Background()
-
-	defer donewg.Done()
-
+func subf(ctx context.Context, vui int) {
 	opts := mqtt.NewClientOptions()
 	opts.AddBroker("192.168.2.29:1883")
 
@@ -83,11 +55,7 @@ func subVuPool(i int, donewg *sync.WaitGroup) {
 	// _ = client.Disconnect(&ctx)
 }
 
-func pubVuPool(i int, donewg *sync.WaitGroup) {
-	ctx := context.Background()
-
-	defer donewg.Done()
-
+func pubf(ctx context.Context, vui int) {
 	opts := mqtt.NewClientOptions()
 	opts.AddBroker("192.168.2.29:1883")
 
@@ -104,9 +72,9 @@ func pubVuPool(i int, donewg *sync.WaitGroup) {
 
 	rate := 1.0 // rps
 	for j := 0; j < int(60*5*rate); j++ {
-		gobench.SleepPoisson(rate)
+		dis.SleepRatePoisson(rate)
 		go func() {
-			_ = client.Publish(&ctx, "fixed/broadcast/topic", 0, gobench.RandomByte(150))
+			_ = client.Publish(&ctx, "fixed/broadcast/topic", 0, dis.RandomByte(150))
 		}()
 	}
 
