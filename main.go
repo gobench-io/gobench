31b034a5ae7d21e50717225640892fd1cf8363ae
nqd <dinh.nguyen@veriksystems.com>
2020-06-17 17:30:40 +0700

    provide usage string

--- main.go
+++ main.go
@@ -7,18 +7,30 @@ import (
 	"github.com/gobench-io/gobench/web"
 )
 
-// func main() {
-// 	bench := server.NewBench()
+// gobench -p 3000 -master -cluster localhost:3001
+// gobench -worker -cluster localhost:5001 -route localhost:3001
 
-// 	bench.Name("mqtt fan out benchmark example")
+var usageStr = `
+Usage: gobench [options]
 
-// 	if err := bench.Start(); err != nil {
-// 		log.Fatalln(err)
-// 	}
+Server Options:
+	-a, --addr <host>		Bind to host address (default: 0.0.0.0)
+	-p, --port <port>		Use port for web client (default: 6891).
+							Worker does not need this option.
+	-m, --master			Run server as master mode
+	-w, --worker 			Run server as worker mode
 
-// 	go benchclient.InternalMonitor()
-// 	web.Serve(bench, 3001)
-// }
+Cluster Options
+		--cluster <port>	Cluster port to solicit and connect (default: 6890)
+							Master is required to have this option
+		--route <host:port> The master address to solicit routes.
+							Every worker must have this option sothat worker can
+							connect to a master
+
+Common Options:
+	-h, --help				Show this message
+	-v, --version			Show version
+`
 
 func main() {
 	server, _ := server.New()
