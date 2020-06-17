fcaecc2b15095d6d4d03901041d60cb52e5359b6
nqd <dinh.nguyen@veriksystems.com>
2020-06-17 16:23:20 +0700

    adding test for node

--- node/node_test.go
+++ node/node_test.go
@@ -6,6 +6,11 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func loadValidPlugin(n *Node) error {
+	so := "./script/valid.so"
+	return n.Load(so)
+}
+
 // func TestNodeWithConfig(t *testing.T) {
 // }
 
@@ -20,8 +25,21 @@ func TestNew(t *testing.T) {
 	assert.Equal(t, n1.status, idle)
 	assert.Nil(t, n1.cancel)
 	assert.Equal(t, n1.units, make(map[string]unit))
+
+	assert.False(t, n1.Running())
 }
 
 func TestLoadPlugin(t *testing.T) {
-
+	n, _ := New()
+	so := "./script/valid.so"
+	assert.Nil(t, n.Load(so))
+	assert.NotNil(t, n.vus)
+	assert.False(t, n.Running())
 }
+
+// func TestRunPlugin(t *testing.T) {
+// 	n, _ := New()
+// 	assert.Nil(t, loadValidPlugin(n))
+// 	assert.Nil(t, n.Run())
+// 	assert.False(t, n.Running())
+// }
