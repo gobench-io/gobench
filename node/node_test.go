5189526014475e6f926d043f6120751f9842e4bf
nqd <dinh.nguyen@veriksystems.com>
2020-06-17 15:04:30 +0700

    adding first node test

---
+++ node/node_test.go
@@ -0,0 +1,23 @@
+package node
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// func TestNodeWithConfig(t *testing.T) {
+// }
+
+func TestNew(t *testing.T) {
+	n1, err := New()
+	assert.Nil(t, err)
+	n2, err := New()
+	assert.Nil(t, err)
+
+	assert.Equal(t, n1, n2)
+
+	assert.Equal(t, n1.status, idle)
+	assert.Nil(t, n1.cancel)
+	assert.Equal(t, n1.units, make(map[string]unit))
+}
