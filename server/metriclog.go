854faf09d26d8261a55fd6129e42221e4286e1ec
nqd <dinh.nguyen@veriksystems.com>
2020-07-16 11:45:48 +0700

    new API to create/find group

--- server/metriclog.go
+++ server/metriclog.go
@@ -3,7 +3,11 @@ package server
 import (
 	"context"
 
+	"github.com/gobench-io/gobench/ent"
+	"github.com/gobench-io/gobench/metrics"
 	gometrics "github.com/rcrowley/go-metrics"
+
+	entGroup "github.com/gobench-io/gobench/ent/group"
 )
 
 func (m *master) Counter(ctx context.Context, wid, title string, time, c int64) error {
@@ -21,4 +25,30 @@ func (m *master) Histogram(ctx context.Context, wid, title string, time int64, h
 
 func (m *master) Gauge(ctx context.Context, wid, title string, time int64, g int64) error {
 	return nil
+}
+
+// NewGroup find or create new group
+// return the existing/new group ent, is created, and error
+func (m *master) NewGroup(ctx context.Context, mg metrics.Group) (*ent.Group, bool, error) {
+	created := false
+
+	eg, err := m.job.app.
+		QueryGroups().
+		Where(
+			entGroup.NameEQ(mg.Name),
+		).
+		Only(ctx)
+
+	// if there is one found
+	if err == nil {
+		return eg, created, err
+	}
+
+	eg, err = m.db.Group.
+		Create().
+		SetName(mg.Name).
+		Save(ctx)
+	created = true
+
+	return eg, created, err
 }
