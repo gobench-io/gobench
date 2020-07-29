04ba6f92f49b782dcb1c034b9ce54e60fba09181
Hieu Le <hieu.lethanh@veriksystems.com>
2020-07-29 15:15:05 +0700

    add version

--- web/ui/react-app/src/components/Title.js
+++ web/ui/react-app/src/components/Title.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import { get } from 'lodash';
+import packageJson from '../../package.json';
 
 const Title = () => <div className="gobench-title">
   <h1 className="app-title">Gobench</h1>
-  <span>A distributed benchmark tool with Golang</span>
+  <span style={{ marginBottom: '8px' }}>A distributed benchmark tool with Golang</span>
+  <span>Version: {get(packageJson, 'version', '')}</span>
 </div>;
 
 export default Title;
