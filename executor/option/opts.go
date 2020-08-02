0284019161981b6c90ab463b82df6ab0bd2d6b7c
nqd <dinh.nguyen@veriksystems.com>
2020-08-02 18:46:35 +0700

    basic parser for executor

--- executor/option/opts.go
+++ executor/option/opts.go
@@ -1,7 +1,47 @@
 package option
 
-import "flag"
+import (
+	"flag"
 
-func ConfigureOptions(fs *flag.FlagSet, args []stringn) (*Options, error) {
+	"github.com/gobench-io/gobench/logger"
+)
 
+type Options struct {
+	Logger       logger.Logger
+	AgentSock    string
+	ExecutorSock string
+	DriverPath   string // the plugin user wrote
+}
+
+func ConfigureOptions(fs *flag.FlagSet, args []string,
+	printVersion, printHelp func()) (opts *Options, err error) {
+	var (
+		showVersion  bool
+		showHelp     bool
+		agentSock    string
+		executorSock string
+		driverPath   string
+	)
+	fs.BoolVar(&showVersion, "v", false, "Print version information")
+	fs.BoolVar(&showVersion, "version", false, "Print version information")
+	fs.BoolVar(&showHelp, "h", false, "Show this message")
+	fs.BoolVar(&showHelp, "help", false, "Show this message")
+
+	fs.StringVar(&agentSock, "agent-sock", "", "Socket of the agent")
+	fs.StringVar(&executorSock, "executor-sock", "", "Socket for this executor")
+	fs.StringVar(&driverPath, "driver-path", "", "Location of the driver plugin")
+
+	if err = fs.Parse(args); err != nil {
+		return
+	}
+	// show version
+	// show help
+
+	opts = &Options{}
+
+	opts.AgentSock = agentSock
+	opts.ExecutorSock = executorSock
+	opts.DriverPath = driverPath
+
+	return
 }
--- executor/option/opts_test.go
+++ executor/option/opts_test.go
@@ -1,6 +1,11 @@
 package option
 
-import "testing"
+import (
+	"flag"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
 
 func TestConfigureOptions(t *testing.T) {
 	type testPrint struct {
@@ -14,8 +19,21 @@ func TestConfigureOptions(t *testing.T) {
 	testFuncs := []testPrint{
 		{[]string{
 			"--agent-sock", "agent/sock",
-			"--exectutor-sock", "executor/soc",
+			"--executor-sock", "executor/sock",
 			"--driver-path", "driver/path",
-		}, version, help}
+		}, version, help},
+	}
+
+	for _, tt := range testFuncs {
+		fs := flag.NewFlagSet("test", flag.ContinueOnError)
+
+		opts, err := ConfigureOptions(fs, tt.args, tt.version, tt.help)
+		assert.Nil(t, err, "err on ConfigureOptions")
+		assert.NotNil(t, opts)
+		assert.Equal(t, Options{
+			AgentSock:    "agent/sock",
+			ExecutorSock: "executor/sock",
+			DriverPath:   "driver/path",
+		}, *opts)
 	}
 }
