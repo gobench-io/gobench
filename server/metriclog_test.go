8988a77c4a429469a812dff0c5c267e74e961405
nqd <dinh.nguyen@veriksystems.com>
2020-08-03 15:18:47 +0700

    test find/create group rpc with duplicate group req

--- server/metriclog_test.go
+++ server/metriclog_test.go
@@ -11,24 +11,23 @@ import (
 )
 
 func TestFindCreateGroupRPC(t *testing.T) {
+	var err error
 	ctx := context.TODO()
 
 	s := seedServer(t)
 
-	_, err := s.NewApplication(ctx, "name", "scenario")
-	assert.Nil(t, err)
-
-	s.master.job.app, err = s.master.nextApplication(ctx)
+	s.master.job.app, err = s.NewApplication(ctx, "name", "scenario")
 	assert.Nil(t, err)
 
 	prefix := time.Now().String()
 	name := "HTTP (" + prefix + ")"
 
 	reply := new(FCGRes)
+	assert.Nil(t, s.master.FindCreateGroupRPC(
+		&FCGArgs{Name: name, AppID: s.master.job.app.ID},
+		reply))
 
-	err = s.master.FindCreateGroupRPC(&FCGArgs{Name: name}, reply)
-	assert.Nil(t, err)
-
+	// read from db, check with reply
 	groups, err := s.master.db.Group.Query().Where(
 		entGroup.Name(name),
 		entGroup.HasApplicationWith(
@@ -39,4 +38,11 @@ func TestFindCreateGroupRPC(t *testing.T) {
 	assert.Len(t, groups, 1)
 	g := groups[0]
 	assert.Equal(t, g.ID, reply.ID)
+
+	// call the same RPC, the result should be like before
+	reply2 := new(FCGRes)
+	assert.Nil(t, s.master.FindCreateGroupRPC(
+		&FCGArgs{Name: name, AppID: s.master.job.app.ID},
+		reply2))
+	assert.Equal(t, reply, reply2)
 }
