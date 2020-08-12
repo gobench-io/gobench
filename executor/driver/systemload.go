33dba8485fecc7c5721efb6fb1d219f1893ec43a
nqd <dinh.nguyen@veriksystems.com>
2020-08-12 17:31:17 +0700

    begin to work on systemload metric for each driver

---
+++ executor/driver/systemload.go
@@ -0,0 +1,14 @@
+package driver
+
+// systemload report the current host system load like cpu, ram, and network
+// status
+
+// systemloadSetup setup the metrics for systemload
+func (d *Driver) systemloadSetup() (err error) {
+	return
+}
+
+// systemloadRun start collect the metrics
+func (d *Driver) systemloadRun() (err error) {
+	return
+}
