510f3720fa13147ef85c4c91729660c31d1adbad
nguyencatpham <dtpham258@gmail.com>
2020-08-12 21:57:38 +0700

    update app clone name

--- web/ui/react-app/src/views/ApplicationsList/Applications.js
+++ web/ui/react-app/src/views/ApplicationsList/Applications.js
@@ -7,42 +7,46 @@ import { RootContext, SpinnerContext } from '../../context'
 
 const Applications = () => {
   const history = useHistory()
-  const	app = useContext(RootContext)
+  const app = useContext(RootContext)
   const applications = app.apps || []
   const isFetching = useContext(SpinnerContext)
-  return <div>
-    <div className='card'>
-      <div className='applications-list-header'>
-        <h2>Applications</h2>
-        <button
-          onClick={() => history.push('/application/create')}
-          className='btn btn-primary'
-        >
-          Create application
-        </button>
-      </div>
-      <div className='applications-body'>
-        {
-          isFetching
-            ? <div>Loading applications</div>
-            : <table className='applications-table'>
+  const now = new Date()
+  const timestamp = `${now.getUTCFullYear()}-${now.getUTCMonth()}-${now.getUTCDate()}-${now.getUTCHours()}-${now.getUTCMinutes()}-${now.getUTCSeconds()}`
+  return (
+    <div>
+      <div className='card'>
+        <div className='applications-list-header'>
+          <h2>Applications</h2>
+          <button
+            onClick={() => history.push('/application/create')}
+            className='btn btn-primary'
+          >
+            Create application
+          </button>
+        </div>
+        <div className='applications-body'>
+          {isFetching ? (
+            <div>Loading applications</div>
+          ) : (
+            <table className='applications-table'>
               <thead>
                 <tr>
                   <th>Application Name</th>
                   <th>Status</th>
-                  <th>Date created</th>
+                  <th>Created At</th>
                   <th />
                 </tr>
               </thead>
               <tbody>
-                {
-                  (!isFetching && applications.length === 0)
-                    ? <tr>
-                      <td colSpan={3}>There is no applications</td>
-                    </tr>
-                    : applications.map((application) => {
-                      const status = get(application, 'status', '')
-                      return <tr key={application.id}>
+                {!isFetching && applications.length === 0 ? (
+                  <tr>
+                    <td colSpan={3}>There is no applications</td>
+                  </tr>
+                ) : (
+                  applications.map((application) => {
+                    const status = get(application, 'status', '')
+                    return (
+                      <tr key={application.id}>
                         <td style={{ width: '34%%' }}>
                           {application.name || '-'}
                         </td>
@@ -54,37 +58,50 @@ const Applications = () => {
                         </td>
                         <td style={{ width: '33%' }}>
                           <div style={{ float: 'right' }}>
-                            {['finished', 'running', 'cancel'].includes(status) &&
+                            {['finished', 'running', 'cancel'].includes(
+                              status
+                            ) && (
                               <button
                                 className='btn btn-primary'
-                                onClick={() => history.push(`/application/${application.id}`)}
+                                onClick={() =>
+                                  history.push(`/application/${application.id}`)}
                               >
                                 View Details
-                              </button>}
-                            {['running', 'pending'].includes(status) &&
+                              </button>
+                            )}
+                            {['running', 'pending'].includes(status) && (
                               <button
                                 className='btn btn-cancel'
-                                onClick={() => app.cancelRunApplication(application.id)}
+                                onClick={() =>
+                                  app.cancelRunApplication(application.id)}
                               >
                                 Cancel
-                              </button>}
-                            {['finished'].includes(status) &&
+                              </button>
+                            )}
+                            {['finished'].includes(status) && (
                               <button
                                 className='btn btn-primary'
-                                onClick={() => app.submitCreate({...application,name:`${application.name}-${Date.UTC()}`})}
+                                onClick={() =>
+                                  app.submitCreate({
+                                    ...application,
+                                    name: `${application.name}-${timestamp}`
+                                  })}
                               >
-                               Clone 
-                              </button>}
+                                Clone
+                              </button>
+                            )}
                           </div>
                         </td>
                       </tr>
-                    })
-                }
+                    )
+                  })
+                )}
               </tbody>
             </table>
-        }
+          )}
+        </div>
       </div>
     </div>
-  </div>
+  )
 }
 export default Applications
