58be823db6422f169ffd8909f57effa3989db133
nqd <dinh.nguyen@veriksystems.com>
2020-08-25 12:22:47 +0700

    executor generator

--- executor/executor_test.go
+++ executor/executor_test.go
@@ -1,6 +1,10 @@
 package executor
 
 import (
+	"io/ioutil"
+	"log"
+	"os"
+	"path/filepath"
 	"testing"
 
 	"github.com/gobench-io/gobench/logger"
@@ -29,4 +33,22 @@ func TestStart(t *testing.T) {
 
 	err = er.Start(&args, reply)
 	assert.Nil(t, err)
+}
+
+func TestGenerate(t *testing.T) {
+	dir, err := ioutil.TempDir("", "scenario-*")
+	assert.Nil(t, err)
+	name := filepath.Join(dir, "main.go")
+	f, err := os.OpenFile(name, os.O_RDWR|os.O_CREATE|os.O_EXCL, 0600)
+	assert.Nil(t, err)
+
+	err = Generate(f, "agentsocket", "executorsocket", 123)
+	assert.Nil(t, err)
+	log.Println("dir", dir)
+	os.Remove(name)
+}
+
+// a generated file should be compiled with a valid scenario
+func TestCompile(t *testing.T) {
+
 }
---
+++ executor/generator.go
@@ -0,0 +1,54 @@
+package executor
+
+import (
+	"html/template"
+	"io"
+)
+
+var tmpl = template.Must(template.
+	New("executor").
+	Parse(`
+package main
+
+import (
+	"context"
+	"log"
+	"time"
+
+	"github.com/gobench-io/gobench/executor"
+	"github.com/gobench-io/gobench/logger"
+	"github.com/gobench-io/gobench/scenario"
+)
+
+func main() {
+	logger := logger.NewStdLogger()
+
+	e, err := executor.NewExecutor(&executor.Options{
+		AgentSock:    "{{ .AgentSock }}",
+		ExecutorSock: "{{ .ExecutorSock }}",
+		AppID:        {{ .AppID }},
+		Vus:          export(),
+	}, logger)
+
+	if err != nil {
+		panic(err)
+	}
+
+	e.Serve()
+}
+`))
+
+// Generate creates an executor go file that is used to compiled to a binary
+func Generate(wr io.Writer, agentSock, executorSock string, appID int) (err error) {
+	type Args struct {
+		AgentSock    string
+		ExecutorSock string
+		AppID        int
+	}
+
+	err = tmpl.Execute(wr, Args{
+		agentSock, executorSock, appID,
+	})
+
+	return
+}
