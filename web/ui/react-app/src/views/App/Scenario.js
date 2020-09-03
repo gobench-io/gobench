6ffa460f72aa3165a0c41acc55cc331b83c73cc4
thoverik <tho.pham@veriksystems.com>
2020-09-03 18:29:32 +0700

    update UI app scenario detail

--- web/ui/react-app/src/views/App/Scenario.js
+++ web/ui/react-app/src/views/App/Scenario.js
@@ -1,33 +1,67 @@
 import React, { useContext } from 'react'
 import { get } from 'lodash'
 import { Divider } from 'antd'
-
+import Editor from 'react-simple-code-editor'
 import { AppContext } from '../../context'
+import { highlight, languages } from 'prismjs/components/prism-core'
+import 'prismjs/components/prism-clike'
+import 'prismjs/components/prism-go'
+import '../../css/editor.css'
 
 const Scenario = () => {
   const appData = useContext(AppContext)
   return <div>
-    <textarea
-      disabled
-      className='application-scenario'
-      value={get(appData, 'scenario', '')}
-    />
+    <div className='application-scenario editor-container'>
+      <Editor
+        value={get(appData, 'scenario', '')}
+        highlight={code => highlight(code, languages.go, 'go')}
+        padding={16}
+        tabSize={4}
+        insertSpaces
+        className='editor'
+        disabled
+        style={{
+          fontFamily: '"Arial", "Open Sans", monospace',
+          fontSize: 14
+        }}
+      />
+    </div>
     <Divider orientation='left' plain>
      gomod
     </Divider>
-    <textarea
-      disabled
-      className='application-gomodule application-scenario'
-      value={get(appData, 'gomod', '')}
-    />
+    <div className='application-scenario editor-container'>
+      <Editor
+        value={get(appData, 'gomod', '')}
+        highlight={code => highlight(code, languages.go, 'go')}
+        padding={16}
+        tabSize={4}
+        insertSpaces
+        className='editor'
+        disabled
+        style={{
+          fontFamily: '"Arial", "Open Sans", monospace',
+          fontSize: 14
+        }}
+      />
+    </div>
     <Divider orientation='left' plain>
      gosum
     </Divider>
-    <textarea
-      disabled
-      className='aapplication-gomodule application-scenario'
-      value={get(appData, 'gosum', '')}
-    />
+    <div className='application-scenario editor-container'>
+      <Editor
+        value={get(appData, 'gosum', '')}
+        highlight={code => highlight(code, languages.go, 'go')}
+        padding={16}
+        tabSize={4}
+        insertSpaces
+        className='editor'
+        disabled
+        style={{
+          fontFamily: '"Arial", "Open Sans", monospace',
+          fontSize: 14
+        }}
+      />
+    </div>
   </div>
 }
 export default Scenario
