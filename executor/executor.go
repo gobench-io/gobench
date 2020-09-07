06b32284259f187ec8a3889af039b9eec79fb32a
nqd <dinh.nguyen@veriksystems.com>
2020-09-07 15:42:01 +0700

    merge driver to executor

--- executor/executor.go
+++ executor/executor.go
@@ -1,15 +1,38 @@
 package executor
 
 import (
+	"context"
+	"errors"
 	"fmt"
 	"net"
 	"os"
+	"sync"
+	"time"
 
+	"github.com/gobench-io/gobench/dis"
 	"github.com/gobench-io/gobench/executor/driver"
 	"github.com/gobench-io/gobench/logger"
+	"github.com/gobench-io/gobench/metrics"
 	"github.com/gobench-io/gobench/pb"
 	"github.com/gobench-io/gobench/scenario"
 	"google.golang.org/grpc"
+)
+
+// executor status. The executor is in either idle, or running state
+type status string
+
+const (
+	Idle    status = "idle"
+	Running status = "running"
+)
+
+// Error
+var (
+	ErrIDNotFound    = errors.New("id not found")
+	ErrNodeIsRunning = errors.New("driver is running")
+
+	ErrAppCancel = errors.New("application is cancel")
+	ErrAppPanic  = errors.New("application is panic")
 )
 
 // Options is for creating new executor object
@@ -22,11 +45,16 @@ type Options struct {
 
 // Executor struct
 type Executor struct {
+	mu           sync.Mutex
 	id           string
 	logger       logger.Logger
 	agentSock    string
 	executorSock string
 	appID        int
+
+	status status
+	vus    scenario.Vus
+	units  map[string]unit //title - gometrics
 
 	driver *driver.Driver
 	rc     pb.AgentClient
@@ -46,7 +74,10 @@ func NewExecutor(opts *Options, logger logger.Logger) (e *Executor, err error) {
 		agentSock:    opts.AgentSock,
 		executorSock: opts.ExecutorSock,
 		appID:        opts.AppID,
+		vus:          opts.Vus,
 	}
+
+	e.status = Idle
 
 	// establishes a connection to agent rpc server
 	socket := "passthrough:///unix://" + e.agentSock
@@ -79,4 +110,144 @@ func (e *Executor) Serve() (err error) {
 	}
 
 	return
+}
+
+func (e *Executor) run(ctx context.Context) (err error) {
+	// first, setup executor's system load
+	if err = e.systemloadSetup(); err != nil {
+		return
+	}
+
+	// todo: check the status
+	if e.status == Running {
+		return ErrNodeIsRunning
+	}
+
+	e.status = Running
+
+	finished := make(chan error)
+
+	// when the runScen finished, we should stop the logScaled and systemloadRun
+	// also; however, not necessary since the executor will be shutdown anyway
+	go e.logScaled(ctx, 10*time.Second)
+	go e.runScen(ctx, finished)
+	go e.systemloadRun(ctx)
+
+	select {
+	case err = <-finished:
+	case <-ctx.Done():
+		err = ErrAppCancel
+	}
+
+	// todo: update status
+
+	return
+}
+
+func (e *Executor) runScen(ctx context.Context, done chan<- error) {
+	var totalVu int
+
+	vus := e.vus
+	for i := range vus {
+		totalVu += vus[i].Nu
+	}
+
+	var wg sync.WaitGroup
+	wg.Add(totalVu)
+
+	for i := range vus {
+		go func(i int) {
+			for j := 0; j < vus[i].Nu; j++ {
+				go func(i, j int) {
+					vus[i].Fu(ctx, j)
+					wg.Done()
+				}(i, j)
+				dis.SleepRatePoisson(vus[i].Rate)
+			}
+		}(i)
+	}
+
+	wg.Wait()
+	done <- nil
+}
+
+// logScaled extract the metric log from a driver
+// should run this function in a routine
+func (e *Executor) logScaled(ctx context.Context, freq time.Duration) {
+	ch := make(chan interface{})
+
+	go func(channel chan interface{}) {
+		for range time.Tick(freq) {
+			channel <- struct{}{}
+		}
+	}(ch)
+
+	if err := d.logScaledOnCue(ctx, ch); err != nil {
+		e.logger.Fatalw("failed logScaledOnCue", "err", err)
+	}
+}
+
+func (e *Executor) logScaledOnCue(ctx context.Context, ch chan interface{}) error {
+	var err error
+	for {
+		select {
+		case <-ch:
+			now := timestampMs()
+			e.mu.Lock()
+			units := e.units
+			e.mu.Unlock()
+
+			for _, u := range units {
+				base := &pb.BasedReqMetric{
+					AppID: int64(e.appID),
+					EID:   e.id,
+					MID:   int64(u.metricID),
+					Time:  now,
+				}
+
+				switch u.Type {
+				case metrics.Counter:
+					_, err = e.rc.Counter(ctx, &pb.CounterReq{
+						Base:  base,
+						Count: u.c.Count(),
+					})
+				case metrics.Histogram:
+					h := u.h.Snapshot()
+					ps := h.Percentiles([]float64{0.5, 0.75, 0.95, 0.99, 0.999})
+					hv := &pb.HistogramValues{
+						Count:  h.Count(),
+						Min:    h.Min(),
+						Max:    h.Max(),
+						Mean:   h.Mean(),
+						Stddev: h.StdDev(),
+						Median: ps[0],
+						P75:    ps[1],
+						P95:    ps[2],
+						P99:    ps[3],
+						P999:   ps[4],
+					}
+					_, err = e.rc.Histogram(ctx, &pb.HistogramReq{
+						Base:      base,
+						Histogram: hv,
+					})
+				case metrics.Gauge:
+					_, err = d.ml.Gauge(ctx, &pb.GaugeReq{
+						Base:  base,
+						Gauge: u.g.Value(),
+					})
+				}
+
+				if err != nil {
+					e.logger.Errorw("metric log failed", "err", err)
+				}
+			}
+		case <-ctx.Done():
+			e.logger.Infow("logScaledOnCue canceled")
+			return nil
+		}
+	}
+}
+
+func timestampMs() int64 {
+	return time.Now().UnixNano() / 1e6 // ms
 }
--- executor/rpc.go
+++ executor/rpc.go
@@ -7,37 +7,6 @@ import (
 	"github.com/gobench-io/gobench/pb"
 )
 
-// RPC is the struct that expose RPC interface
-// avoid the rpc.Register: method "xxx" has 1 input parameters; needs exactly three
-type RPC struct {
-	e *Executor
-}
-
-func newExecutorRPC(e *Executor) (er *RPC, err error) {
-	return &RPC{
-		e: e,
-	}, nil
-}
-
-func (er *RPC) Start(req *bool, res *bool) (err error) {
-	ctx := context.TODO()
-
-	er.e.logger.Infow("executor rpc starting")
-
-	err = er.e.driver.Run(ctx)
-
-	er.e.logger.Infow("executor rpc finished")
-
-	return
-}
-
-// Terminate shutdown this executor process
-func (er *RPC) Terminate(req *int, res *bool) (err error) {
-	os.Exit(*req)
-	*res = true
-	return
-}
-
 // Start begins to run the program
 func (m *Executor) Start(ctx context.Context, req *pb.StartRequest) (*pb.StartResult, error) {
 	m.logger.Infow("executor rpc starting")
