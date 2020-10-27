4463efbcd7c7d53d874f15b9bb99224eebdcc303
nqd <dinh.nguyen@veriksystems.com>
2020-10-27 17:16:41 +0700

    comment

--- web/healthz.go
+++ web/healthz.go
@@ -6,14 +6,16 @@ import (
 	"github.com/go-chi/render"
 )
 
+// healthz checks status of the master by
+// - ping the db
 func (h *handler) healthz(w http.ResponseWriter, r *http.Request) {
+	// ping
 	err := h.s.PingDb()
 	if err != nil {
 		render.Render(w, r, ErrInternalServer(err))
 		return
 	}
 
-	// ping
 	render.Respond(w, r, render.M{
 		"hostname": h.s.GetHostname(),
 	})
