b19defea77cdd123b2dee9d80eaf384c54aafe06
Dinh <nqdinhddt@gmail.com>
2021-07-11 09:23:13 +0700

    test grpc setup method

--- clients/grpc/grpc_test.go
+++ clients/grpc/grpc_test.go
@@ -3,35 +3,70 @@ package gbGrpc
 import (
 	"testing"
 
+	"github.com/gobench-io/gobench/executor"
 	"github.com/gobench-io/gobench/executor/metrics"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
 )
 
-// TODO: need to mock executorInstance
-// executor.SetInstance(executor interface)
+type MockClientConnect struct {
+	mock.Mock
+}
+
+func (m *MockClientConnect) Setup(groups []metrics.Group) error {
+	args := m.Called(groups)
+	return args.Error(0)
+}
+
+func (m *MockClientConnect) Notify(title string, value int64) error {
+	args := m.Called(title, value)
+	return args.Error(0)
+}
+
 func TestGbClientConnSetupMethod(t *testing.T) {
+	testClientConnect := new(MockClientConnect)
+	executor.SetClientConnect(testClientConnect)
+
+	expectedGroupsArg := []metrics.Group{
+		{
+			Name: "gRPC (host.io)",
+			Graphs: []metrics.Graph{
+				{
+					Title: "gRPC Response",
+					Unit:  "N",
+					Metrics: []metrics.Metric{
+						{
+							Title: "get.foo.grpc_ok", // success
+							Type:  metrics.Counter,
+						},
+						{
+							Title: "get.foo.grpc_fail", // fail
+							Type:  metrics.Counter,
+						},
+					},
+				},
+				{
+					Title: "Latency",
+					Unit:  "Microsecond",
+					Metrics: []metrics.Metric{
+						{
+							Title: "get.foo.latency", // latency
+							Type:  metrics.Histogram,
+						},
+					},
+				},
+			},
+		},
+	}
+
+	testClientConnect.On("Setup", expectedGroupsArg).Return(nil)
+
 	conn := &GbClientConn{
 		methodGraphsMap: make(map[string][]metrics.Graph),
 		target:          "host.io",
 	}
 
-	method := "get.foo"
-	graphs, err := conn.setupMethod(method)
+	actualGraphs, err := conn.setupMethod("get.foo")
 	assert.Nil(t, err)
-	assert.Equal(t, graphs, []metrics.Graph{
-		{
-			Title: "gRPC Response",
-			Unit:  "N",
-			Metrics: []metrics.Metric{
-				{
-					Title: method + ".grpc_ok", // success
-					Type:  metrics.Counter,
-				},
-				{
-					Title: method + ".grpc_fail", // fail
-					Type:  metrics.Counter,
-				},
-			},
-		},
-	})
+	assert.Equal(t, expectedGroupsArg[0].Graphs, actualGraphs)
 }
